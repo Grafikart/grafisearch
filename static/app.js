69c6a3a63168eab3d9137ad2a3aeb5ea9f7f3875
jonathan <contact@grafikart.fr>
2022-04-03 00:05:54 +0200

    feat: poc css

--- static/app.js
+++ static/app.js
@@ -15,17 +15,28 @@ form.addEventListener('submit', (e) => {
  * @param {string} q
  */
 function search(q) {
+    document.body.classList.add('has-results')
     fetch(`/google?q=${q}`).then(r => r.json()).then(injectResult('#google'))    
 }
 
 const injectResult = (selector) => (results) => {
     const container = document.querySelector(selector)
-    container.innerHTML = results.map(r => `<div class="result">
-        <h2 class="result__title">${r.title}</h2>
-        <em>${r.url.replace('https://', '')}</em>
-        <p>${r.desc}</p>
-        <a class="result__link" href="${r.url}"></a>
-    </div>`).join('')
+    container.innerHTML = results.map(r => {
+        const url = new URL(r.url)
+        const favicon = `https://external-content.duckduckgo.com/ip3/${url.host}.ico`
+        const domain = r.url
+            .replace('https://', '')
+            .replace(/\/$/, '')
+        return `<div class="result">
+            <a class="result__title" href="${r.url}">${r.title}</a>
+            <a class="result__url">
+                <img src="${favicon}" alt="">
+                <span>${domain}</span>
+            </a>
+            <p>${r.desc}</p>
+            <a class="result__link" href="${r.url}"></a>
+        </div>`
+    }).join('')
 }
 
 const q = url.searchParams.get('q')
