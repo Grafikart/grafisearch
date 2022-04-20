490ea9ffcf66c5edb735ebd5085f404c12d132b5
Jonathan <contact@grafikart.fr>
2022-04-20 09:44:13 +0200

    fix: github bang redirect
    Fix #5

--- bangs.go
+++ bangs.go
@@ -17,7 +17,7 @@ var redirectBangs = map[string]string{
 	"!npm":  "https://www.npmjs.com/search?q=%s",
 	"!tr":   "https://www.wordreference.com/enfr/%s",
 	"!imdb": "https://www.imdb.com/find?s=all&q=%s",
-	"!gh":   "https://github.com/search?utf8=%E2%9C%93&q=%s",
+	"!gh":   "https://github.com/search?q=%s",
 	"!yt":   "https://www.youtube.com/results?search_query=%s",
 	"!d":    "https://www.larousse.fr/dictionnaires/francais/%s",
 	"!g":    "https://www.google.com/search?q=%s",
