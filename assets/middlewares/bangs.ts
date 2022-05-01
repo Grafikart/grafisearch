8995f1849a89c57778e09c5a5c54592bff801d5a
hanako-eo <enuxura@gmail.com>
2022-05-01 19:46:55 +0200

    fix: clear bang in middle of string

--- assets/middlewares/bangs.ts
+++ assets/middlewares/bangs.ts
@@ -1,21 +1,24 @@
 export function bangs(q: string): boolean {
-	if (!q.includes('!') || !window.BANGS) {
-		return false
-	}
-	for (let bang of Object.keys(window.BANGS)) {
-		if (q.startsWith(bang + ' ') || q.endsWith(' ' + bang)) {
-			const search = q.replace(bang, '').trim()
-			const url = window.BANGS[bang]
-			if (url) {
-				window.location.assign(
-					url.replace(
-						'%s',
-						encodeURIComponent(search.replaceAll(bang, '').trim())
-					)
-				)
-				return true
-			}
-		}
-	}
-	return false
+  if (!q.includes('!') || !window.BANGS) {
+    return false
+  }
+  for (let bang of Object.keys(window.BANGS)) {
+    // regex for test if the query start or finish with the bang
+    const bangRe = RegExp(`^${bang} *| *${bang}$`)
+    if (bangRe.test(q)) {
+      // clear the bang
+      const search = q.replace(bangRe, '')
+      const url = window.BANGS[bang]
+      if (url) {
+        window.location.assign(
+          url.replace(
+            '%s',
+            encodeURIComponent(search)
+          )
+        )
+        return true
+      }
+    }
+  }
+  return false
 }
--- assets/middlewares/uppercase.ts
+++ assets/middlewares/uppercase.ts
@@ -2,12 +2,14 @@ const headerText = document.querySelector('.header-text') as HTMLDivElement
 let hasResult = false
 
 export function uppercase(q: string): boolean {
-	if (q.includes('!maj')) {
-		headerText.innerText = ` = ${q.replaceAll('!maj', '').trim().toUpperCase()}`
-		return true
-	} else if (hasResult) {
-		headerText.innerHTML = ''
-		hasResult = false
-	}
-	return false
+  const majRe = /^!maj *| *!maj$/
+  if (majRe.test(q)) {
+    headerText.innerText = ` = ${q.replace(majRe, '').toUpperCase()}`
+    hasResult = true
+    return true
+  } else if (hasResult) {
+    headerText.innerHTML = ''
+    hasResult = false
+  }
+  return false
 }
